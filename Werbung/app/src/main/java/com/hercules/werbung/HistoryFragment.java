40f1e15e56f6e3c795a1ea6ec0d6d23f8ad5b726
chinmaykrishna <seekay3@gmail.com>
2015-10-26 11:40:53 +0530

    getting issues for user from api

--- Werbung/app/src/main/java/com/hercules/werbung/HistoryFragment.java
+++ Werbung/app/src/main/java/com/hercules/werbung/HistoryFragment.java
@@ -3,14 +3,27 @@ package com.hercules.werbung;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.hercules.werbung.jirarestapi.JiraRestClient;
+import com.hercules.werbung.jirarestapi.com.JiraListener;
+
+import org.json.JSONObject;
 
 /**
  * History fragment placeholder
  */
 public class HistoryFragment extends Fragment {
+    public static final String BASE_URL = "https://adqa-dev.atlassian.net";
+    public static final String USERNAME = "topcoder";
+    public static final String PASSWORD = "pass1234";
+
+    // Singleton of the Jira REST API lib
+    private JiraRestClient mJiraRestClient;
+
 
     /**
      * Returns a new instance of this fragment for the given section number.
@@ -28,7 +41,25 @@ public class HistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_history, container,
                 false);
+        mJiraRestClient = JiraRestClient.getInstance(getActivity());
+        mJiraRestClient.setBaseURL(BASE_URL);
+        getUserIssues();
         return rootView;
+    }
+
+    private void getUserIssues(){
+        mJiraRestClient.search(USERNAME, PASSWORD, "assignee=" + USERNAME, null, null, null, null, null, new JiraListener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jiraObject) {
+                Log.d("UserIssues",jiraObject.toString());
+
+            }
+
+            @Override
+            public void onErrorResponse(Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
